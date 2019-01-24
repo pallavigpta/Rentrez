77a333349acc7d268d6e35310e857e3d996c620e
Pallavi Gutpa <14pg12@queensu.ca>
2019-01-24 17:35:58 -0500

    clean up data frame output

--- Download.R
+++ Download.R
@@ -8,7 +8,10 @@ ncbi_ids <- c("HQ433692.1","HQ433694.1","HQ433691.1") # store gene accession num
 Bburg<-entrez_fetch(db = "nuccore", id = ncbi_ids, rettype = "fasta") # retrieve gene sequences from NCBI
 
 Bburgseq <- strsplit(Bburg, "\n\n")[[1]] # create a vector with 3 sequences
-Bburgseq <- gsub(".*sequence|\n", "", Bburgseq) # clean up sequences
-Bburgseq # check
+Sequences <- gsub(".*sequence|\n", "", Bburgseq) # clean up sequences
+Sequences # check
 
-write.csv(Bburgseq, file = "Sequences.csv") # output vector into a csv file
+df <- as.data.frame(Sequences, row.names=ncbi_ids) # set vector into data frame
+df
+
+write.csv(df, file = "Sequences.csv") # output vector into a csv file
